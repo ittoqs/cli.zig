19771dce9d5414819eef81dc9ccfecb37e5efec3
Ampolleng <cjoscarpone@gmail.com>
2026-07-23 00:18:49 +0800

    Delete test_child.zig

--- test_child.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    const TypeInfo = @typeInfo(std.process.Child.StdIo);
-    inline for (TypeInfo.Enum.fields) |field| {
-        std.debug.print("Field: {s}\n", .{field.name});
-    }
-}
